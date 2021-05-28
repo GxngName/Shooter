fab6d9b4a94771dfcf63d01d6ccbab99b24eb591
GxngName <76249868+GxngName@users.noreply.github.com>
2021-05-28 20:10:12 +0300

    28/05
    не работатет

--- 1main.py
+++ 1main.py
@@ -4,8 +4,8 @@ from os import path
 
 img_dir = path.join(path.dirname(__file__), 'img')
 
-WIDTH = 600
-HEIGHT = 720
+WIDTH = 480
+HEIGHT = 600
 FPS = 60
 
 WHITE = (255,) * 3
@@ -15,14 +15,13 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-score = 0
-font_name = 'arial'
 pygame.init()
 pygame.mixer.init() 
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("Welcome to the club buddy!")
 background_color = (0,) * 3
 clock = pygame.time.Clock()
+   
 def draw_text(surf, font_name, text, size, x, y):
     font = pygame.font.SysFont(font_name, size)
     text_surface = font.render(text, True, WHITE)
@@ -30,12 +29,13 @@ def draw_text(surf, font_name, text, size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
-
 def newmob():
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
 
+score = 0
+font_name = 'arial'
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -61,12 +61,11 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
-    
+            
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
-        shoot_sound.play()
 
 
 class Mob(pygame.sprite.Sprite):
@@ -77,7 +76,7 @@ class Mob(pygame.sprite.Sprite):
         self.image_orig.set_colorkey(transColor)
         self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
-        self.radius = int(self.rect.width * .85 / 2)
+        self.radius = int(self.rect.width * .85 / 4)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-150, -100)
         self.speedy = random.randrange(1, 8)
@@ -86,17 +85,19 @@ class Mob(pygame.sprite.Sprite):
         self.rot_speed = random.randrange(-8, 8)
         self.last_update = pygame.time.get_ticks()
 
-    def rotate(self):
-        now = pygame.time.get_ticks()
-        if now - self.last_update > 50:
-            self.last_update = now
-            self.rot = (self.rot + self.rot_speed) % 360
-            new_image = pygame.transform.rotate(self.image_orig, self.rot)
-            old_center = self.rect.center
-            self.image = new_image
-            self.rect = self.image.get_rect()
-            self.rect.center = old_center
-    
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+            self.speedx = random.randrange(-3, 3)
+            self.rot = 0
+            self.rot_speed = random.randrange(-8, 8)
+            self.last_update = pygame.time.get_ticks() 
+
     def rotate(self):
         now = pygame.time.get_ticks()
         if now - self.last_update > 50:
@@ -111,8 +112,8 @@ class Mob(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -123,14 +124,14 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
-background = pygame.image.load('cosmos.png').convert()
+background = pygame.image.load("cosmos.png").convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load("ship.png").convert()
-bullet_img = pygame.image.load("bullet.png").convert()
+bullet_img = pygame.image.load("pixilart-drawing (5).png").convert()
 meteor_images = []
-meteor_list = ['meteor.png', 'meteor2.png'] 
+meteor_list = ['pixilart-drawing (2).png'] 
 for img in meteor_list:
-    meteor_images.append(pygame.image.load(img).convert())
+    meteor_images.append(pygame.image.load(img).convert()) 
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -151,28 +152,69 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 player.shoot()
+    
     all_sprites.update()
 
-    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
-    for hit in hits:
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
 
-    hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
-    for hit in hits:
-        player.shield -= hit.radius * 2
-        newmob()
-        if player.shield <= 0:
-            running = False
+hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+for hit in hits:
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    if hits:
+        running = False
 
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
     draw_text(screen, 'arial', str(score), 18, WIDTH / 2, 10)
-    #draw_shield_bar(screen, 5, 5, player.shield)
-
+    
     pygame.display.update()
 
+
+ 
+pygame.init()
+
+win_size = 700
+win = pygame.display.set_mode((win_size,win_size))
+background_color = (255,255,255)
+
+
+font0 = pygame.font.SysFont('arial', 64)
+
+
+def draw_text(text, font, color, frame, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x,y)
+    frame.blit(textobj, textrect)  
+
+menu_color = (0,) * 3
+def menu(win):
+    running = True
+    while running:
+        events = pygame.event.get()
+        for event in events:
+           if event.type == pygame.QUIT:
+                exit()
+           if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        win.fill(menu_color)
+        draw_text('Menu', font0, RED, win, 320, 320)
+        pygame.display.update()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                menu(win)
+
+    win.fill(background_color)
+
+    pygame.display.update()
 
 pygame.quit()
